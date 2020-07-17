1700bab4ca976cfef2494b4ea6327b56d8262f36
SmartDigitalLatam <bu.digital@outlook.com>
2020-07-17 17:10:13 -0300

    headspace backend v.2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const CosmosClientInterface = require("@azure/cosmos").CosmosClient;
 const app = express()
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header("Access-Control-Allow-Origin", "https://headspace-portal.azurewebsites.net");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
